2ea258bf57b68594226bc9061f665ffe63780ff5
atton <e115763@gmail.com>
2012-10-28 17:13:59 +0900

    add teacher's sample

--- src/e115763/FileWrite.java
+++ src/e115763/FileWrite.java
@@ -1,10 +1,43 @@
 package e115763;
 
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.FileChannel;
+
 public class FileWrite {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		File f = new File("test");
+		try {
+			FileOutputStream os = new FileOutputStream(f);
+			FileChannel oc = os.getChannel();
+			os.close();
+			int size = 1000000000;
+			int bufsize = 1024;
+			for(int remain = size; remain > 0;remain -= bufsize) {
+				writeTest(oc,bufsize);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	private static void writeTest(FileChannel oc, int size){
+		ByteBuffer srcs = generateTestData('c',size);
+		try {
+			oc.write(srcs);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static ByteBuffer generateTestData(char c, int size) {
+		ByteBuffer buf = ByteBuffer.allocate(size);
+		for(int i=0; i < size; i++) {
+			buf.put((byte)c);
+		}
+		buf.flip();
+		return buf;
+	}
 }
