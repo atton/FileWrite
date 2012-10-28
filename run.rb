c79b8f7e6c285ca6310ed4172e65f95de872f50c
atton <e115763@gmail.com>
2012-10-29 08:14:15 +0900

    show run dialog in run script

--- run.rb
+++ run.rb
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
+# buffers は 10倍 していくと一晩放置しても終わらなかった
 
 RunFile = "target/FileWrite-0.0.1-SNAPSHOT.jar"
 
 outputfile = "test_file"
-buffers    = [1, 10, 100, 1000, 10000, 100000, 1000000]
-file_sizes = [0, 100 , 10000, 1000000, 100000000, 10000000000]
+buffers    = [1, 100, 10000, 1000000]
+file_sizes = [0, 100, 10000, 1000000, 100000000, 10000000000]
 string     = "hoge"
 
 buffers.each do |buffer|
   File.open("result/#{buffer}.txt","w") do |file|
     file_sizes.each do |size|
+      puts "run : buffer #{buffer}, size #{size}"
       time = Time.now
       system("java -jar #{RunFile} -b #{buffer} -c #{string} -s #{size} -n #{outputfile}")
       diff =  Time.now - time
