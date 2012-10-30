566d22a0dce85ba62d8e9f4a3ed278893d45c82f
atton <e115763@gmail.com>
2012-10-30 12:00:04 +0900

    add test

--- src/e115763/FileWriteTest.java
+++ src/e115763/FileWriteTest.java
@@ -12,4 +12,11 @@ public class FileWriteTest {
 		assertEquals("1024", FileWrite.parseArgs(testArg, "-b"));
 		assertEquals("1024", FileWrite.parseArgs(testArg, "-s"));
 	}
+	@Test public void parseArgsTestGetValue() {
+		String[] testArg = {"-c", "string", "-s", "3000", "-b", "50", "-n", "filename"};
+		assertEquals("filename",   FileWrite.parseArgs(testArg, "-n"));
+		assertEquals("50", FileWrite.parseArgs(testArg, "-b"));
+		assertEquals("3000", FileWrite.parseArgs(testArg, "-s"));
+		assertEquals("string", FileWrite.parseArgs(testArg, "-c"));
+	}
 }
