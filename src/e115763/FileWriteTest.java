f845f1d388592b602bf45c6e98ddd6c0ea864138
atton <e115763@gmail.com>
2012-11-01 17:59:33 +0900

    delete parseArg test, add file create test

--- src/e115763/FileWriteTest.java
+++ src/e115763/FileWriteTest.java
@@ -2,21 +2,18 @@ package e115763;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
 
 public class FileWriteTest {
 	
-	@Test public void parseArgsTestEmptyArgs() {
-		String[] testArg = {""};
-		assertEquals(null,   FileWrite.parseArgs(testArg, "-n"));
-		assertEquals("1024", FileWrite.parseArgs(testArg, "-b"));
-		assertEquals("1024", FileWrite.parseArgs(testArg, "-s"));
-	}
-	@Test public void parseArgsTestGetValue() {
-		String[] testArg = {"-c", "string", "-s", "3000", "-b", "50", "-n", "filename"};
-		assertEquals("filename",   FileWrite.parseArgs(testArg, "-n"));
-		assertEquals("50", FileWrite.parseArgs(testArg, "-b"));
-		assertEquals("3000", FileWrite.parseArgs(testArg, "-s"));
-		assertEquals("string", FileWrite.parseArgs(testArg, "-c"));
+	@Test public void FileWriteRunTest() {
+		String filename = "test";
+		String[] testArg = {"-c", "string", "-s", "3000", "-b", "50", "-n", filename};
+		FileWrite.main(testArg);
+		File file = new File(filename);
+		assert(file.exists());
+		file.delete();
 	}
 }
