d67546d276129faa17bc0c7192f2437d833cb1e6
atton <e115763@gmail.com>
2012-11-01 16:38:42 +0900

    change graph output to pdf from eps

--- plot.sh
+++ plot.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+file_name="result/graph"
+
 gnuplot <<-EOF
 set terminal postscript eps
-set output "result/graph.eps"
+set output "${file_name}.eps"
 set xlabel "file size (log scale)"
 set logscale x
 set ylabel "time (log scale)"
@@ -10,3 +12,6 @@ set logscale y
 set title "file generate times to change buffer size"
 plot "result/1.txt" with lines, "result/100.txt" with lines, "result/10000.txt" with lines, "result/1000000.txt" with lines
 EOF
+
+epstopdf ${file_name}.eps
+rm ${file_name}.eps
